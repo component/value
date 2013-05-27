99b0a13c65d686f9f283ae7824cfc2a1abae670a
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-05-27 15:30:51 -0700

    added select support

--- test/tests.js
+++ test/tests.js
@@ -24,7 +24,6 @@ describe('textareas', function(){
 describe('checkboxes', function(){
   it('should check when true', function(){
     var el = dom('<input type="checkbox">');
-
     value(el.get(0), true);
     assert(true === el.get(0).checked);
     assert(true === value(el.get(0)));
@@ -32,7 +31,6 @@ describe('checkboxes', function(){
 
   it('should uncheck when false', function(){
     var el = dom('<input type="checkbox" checked="checked">');
-
     value(el.get(0), false);
     assert(false === el.get(0).checked);
     assert(false === value(el.get(0)));
@@ -41,9 +39,31 @@ describe('checkboxes', function(){
   describe('with a value', function(){
     it('should return the value', function(){
       var el = dom('<input type="checkbox" value="accepted">');
-
       value(el.get(0), true);;
       assert('accepted' === value(el.get(0)));
+    })
+  })
+})
+
+describe('selects', function(){
+  it('should select an option', function(){
+    var el = dom('<select><option value="a"></option><option value="b"></option></select>');
+    value(el.get(0), 'b');
+    assert(true === el.find('option').get(1).selected);
+    assert('b' === value(el.get(0)));
+  })
+
+  describe('with a selected option', function(){
+    it('should return the options value', function(){
+      var el = dom('<select><option value="a"></option><option value="b" selected></option></select>');
+      assert('b' === value(el.get(0)));
+    })
+  })
+
+  describe('without a selected option', function(){
+    it('should return the first options value', function(){
+      var el = dom('<select><option value="a"></option><option value="b"></option></select>');
+      assert('a' === value(el.get(0)));
     })
   })
 })
